74d7e56d84e0c63c61142ed5cd390ba520efe9c4
Yaron Koren <yaron@users.mediawiki.org>
2011-02-16 03:04:27 +0000

    Added handling for regular expressions

--- ReplaceTextJob.php
+++ ReplaceTextJob.php
@@ -1,10 +1,11 @@
-<?php
+﻿<?php
 
 /**
  * Background job to replace text in a given page
  * - based on /includes/RefreshLinksJob.php
  *
  * @author Yaron Koren
+ * @author Ankit Garg
  */
 class ReplaceTextJob extends Job {
 
@@ -30,7 +31,12 @@ class ReplaceTextJob extends Job {
 			$actual_user = $wgUser;
 			$wgUser = User::newFromId( $this->params['user_id'] );
 			$cur_page_name = $this->title->getText();
-			$new_page_name = str_replace( $this->params['target_str'], $this->params['replacement_str'], $cur_page_name );
+			if ( $this->params['use_regex'] ) {
+				$new_page_name = preg_replace( "/".$this->params['target_str']."/U", $this->params['replacement_str'], $cur_page_name );
+			} else {
+				$new_page_name = str_replace( $this->params['target_str'], $this->params['replacement_str'], $cur_page_name );
+			}
+
 			$new_title = Title::newFromText( $new_page_name, $this->title->getNamespace() );
 			$reason = $this->params['edit_summary'];
 			$create_redirect = $this->params['create_redirect'];
@@ -53,7 +59,13 @@ class ReplaceTextJob extends Job {
 			$target_str = $this->params['target_str'];
 			$replacement_str = $this->params['replacement_str'];
 			$num_matches;
-			$new_text = str_replace( $target_str, $replacement_str, $article_text, $num_matches );
+
+			if ( $this->params['use_regex'] ) {
+				$new_text = preg_replace( '/'.$target_str.'/U', $replacement_str, $article_text, -1, $num_matches );
+			} else {
+				$new_text = str_replace( $target_str, $replacement_str, $article_text, $num_matches );
+			}
+
 			// if there's at least one replacement, modify the page,
 			// using the passed-in edit summary
 			if ( $num_matches > 0 ) {
