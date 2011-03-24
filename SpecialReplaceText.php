34a416305d82cadcf3e72aa2de08261d226cfb22
Chad Horohoe <demon@users.mediawiki.org>
2011-03-24 17:38:40 +0000

    Partial revert r84686: Yaron wants to maintain b/c in these still

--- SpecialReplaceText.php
+++ SpecialReplaceText.php
@@ -6,7 +6,11 @@ class ReplaceText extends SpecialPage {
 	 * Constructor
 	 */
 	public function __construct() {
+		global $wgVersion;
 		parent::__construct( 'ReplaceText', 'replacetext' );
+		if ( version_compare( $wgVersion, '1.16', '<' ) ) {
+			wfLoadExtensionMessages( 'ReplaceText' );
+		}
 	}
 
 	function execute( $query ) {
