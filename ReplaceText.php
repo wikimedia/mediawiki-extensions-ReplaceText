70e45385bf4f7973d827e8156be3bf5f24b02aed
Reedy <reedy@wikimedia.org>
2015-08-14 22:39:07 +0100

    Fix mistakes from Ia90e96b68e4bfe4
    Change-Id: I40f21369a1e9e1f44417fc50e0c8bc23da117a3e

--- ReplaceText.php
+++ ReplaceText.php
@@ -21,9 +21,9 @@ if ( function_exists( 'wfLoadExtension' ) ) {
 	wfLoadExtension( 'ReplaceText' );
 	// Keep i18n globals so mergeMessageFileList.php doesn't break
 	$wgMessagesDirs['ReplaceText'] = __DIR__ . '/i18n';
-	$wgExtensionMessagesFiles['ReplaceTextMagic'] = __DIR__ . '/ReplaceText.i18n.magic.php';
+	$wgExtensionMessagesFiles['ReplaceTextAlias'] = __DIR__ . '/ReplaceText.alias.php';
 	/* wfWarn(
-	'Deprecated PHP entry point used for External Data extension. Please use wfLoadExtension instead, ' .
+	'Deprecated PHP entry point used for Replace Text extension. Please use wfLoadExtension instead, ' .
 	'see https://www.mediawiki.org/wiki/Extension_registration for more details.'
 	); */
 	return;
