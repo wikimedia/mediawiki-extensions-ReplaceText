75ddb5fea0bdfe6d928b0a25b3cebeb42fcf87d0
Fomafix <fomafix@googlemail.com>
2018-12-06 18:27:46 +0100

    Use private function instead of attaching the function to window
    Change-Id: Ie6f8d0cdf0cf8842ff756839951b4b4bdbade942

--- resources/ext.ReplaceText.js
+++ resources/ext.ReplaceText.js
@@ -1,27 +1,25 @@
-window.invertSelections = function () {
+( function ( $ ) {
 	'use strict';
 
-	var form = document.getElementById( 'choose_pages' ),
-		numElements = form.elements.length,
-		i,
-		curElement;
+	function invertSelections() {
+		var form = document.getElementById( 'choose_pages' ),
+			numElements = form.elements.length,
+			i,
+			curElement;
 
-	for ( i = 0; i < numElements; i++ ) {
-		curElement = form.elements[ i ];
+		for ( i = 0; i < numElements; i++ ) {
+			curElement = form.elements[ i ];
 
-		if ( curElement.type === 'checkbox' && curElement.id !== 'create-redirect' &&
-			curElement.id !== 'watch-pages' && curElement.id !== 'doAnnounce' ) {
-			form.elements[ i ].checked = form.elements[ i ].checked !== true;
+			if ( curElement.type === 'checkbox' && curElement.id !== 'create-redirect' &&
+				curElement.id !== 'watch-pages' && curElement.id !== 'doAnnounce' ) {
+				form.elements[ i ].checked = form.elements[ i ].checked !== true;
+			}
 		}
 	}
-};
 
-( function ( $ ) {
 	$( function () {
 		$( '#replacetext-invert' )
-			.on( 'click', function () {
-				window.invertSelections();
-			} )
+			.on( 'click', invertSelections )
 			.prop( 'disabled', false );
 	} );
 }( jQuery ) );
