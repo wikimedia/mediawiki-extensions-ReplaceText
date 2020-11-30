wikimedia/mediawiki-extensions-ReplaceText

commit bcd6b2f732824d26a55ff3892442e143df7994a6

Fix for 7f823dfccbc4 - restore support for PostgreSQL
Change-Id: I7de2461fd7e99ce53d5d2cebfe965123c91fbdae

src/ReplaceTextSearch.php
@@ -51,7 +51,7 @@ class ReplaceTextSearch {
 			'rev_id = page_latest',
 			'rev_id = slot_revision_id',
 			'slot_content_id = content_id',
-			'SUBSTRING(content_address, 4) = old_id'
+			'CAST(SUBSTRING(content_address, 4) AS INTEGER) = old_id'
 		];
 
 		self::categoryCondition( $category, $tables, $conds );

commit 286c538e6f7c8923dbc48b76964e1a9c23bb1972

Fix MySQL invalid syntax regression from bcd6b2f7
Casting "AS INTEGER" is not valid MySQL syntax. You have to cast "AS
SIGNED" or "AS UNSIGNED".

Change-Id: Ia84f135a8699fdc6cff6877c31fae1d46bf84f97

src/ReplaceTextSearch.php
@@ -51,7 +51,7 @@ class ReplaceTextSearch {
 			'rev_id = page_latest',
 			'rev_id = slot_revision_id',
 			'slot_content_id = content_id',
-			'CAST(SUBSTRING(content_address, 4) AS INTEGER) = old_id'
+			'CAST(SUBSTRING(content_address, 4) AS SIGNED) = old_id'
 		];
 
 		self::categoryCondition( $category, $tables, $conds );