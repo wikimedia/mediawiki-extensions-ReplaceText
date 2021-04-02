b15a7352d3c085b39de440a8cf929e5c14743cf7
Mark A. Hershberger <mah@nichework.com>
2021-04-02 16:34:01 -0400

    Provide phan with stubs for AL* classes
    Bug: T279202
    Change-Id: Ia1e667935c2553a1bff999516e3ad349d4923e21

---
+++ .phan/stubs/ALRow.php
@@ -0,0 +1,10 @@
+<?php
+
+class ALRow {
+	public $name;
+	public $items;
+
+	function __construct( $name );
+	function addItem( $item, $next_item_label = null );
+	function toString();
+}
---
+++ .phan/stubs/ALTree.php
@@ -0,0 +1,10 @@
+<?php
+
+class ALTree {
+	public $sections;
+
+	function __construct();
+	function getSection( $section_header );
+	function addSection( $section, $next_section_header = null );
+	function toString();
+}
