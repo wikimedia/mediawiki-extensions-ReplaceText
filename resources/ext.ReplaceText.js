75785d4957b6ed3bed3b07c6414bc5de3fe0d3ee
Fomafix <fomafix@googlemail.com>
2025-04-07 06:02:43 +0000

    Use ES6 and simplify JavaScript code
    Bug: T334853
    Change-Id: I3b9f2d340a3714a82a63b5624787d136d6761bf1

--- resources/ext.ReplaceText.js
+++ resources/ext.ReplaceText.js
@@ -2,19 +2,9 @@
 	'use strict';
 
 	function invertSelections() {
-		var form = document.getElementById( 'choose_pages' ),
-			numElements = form.elements.length,
-			i,
-			curElement;
-
-		for ( i = 0; i < numElements; i++ ) {
-			curElement = form.elements[ i ];
-
-			if ( curElement.type === 'checkbox' && curElement.id !== 'create-redirect' &&
-				curElement.id !== 'watch-pages' && curElement.id !== 'botEdit' ) {
-				curElement.checked = !curElement.checked;
-			}
-		}
+		$( '.oo-ui-inputWidget-input[type=checkbox]' ).each( ( _i, checkbox ) => {
+			checkbox.checked = !checkbox.checked;
+		} );
 	}
 
 	/**
@@ -23,43 +13,35 @@
 	 * Uses jQuery#codePointLimit to enforce the limit.
 	 *
 	 * @param {OO.ui.TextInputWidget} textInputWidget Text input widget
-	 * @param {number} [limit] Code point limit, defaults to $input's maxlength
-	 * @param {Function} [filterFunction] Function to call on the string before assessing the length.
 	 */
-	var visibleCodePointLimit = function ( textInputWidget ) {
-		var limit = +textInputWidget.$input.attr( 'maxlength' );
-		var codePointLength = require( 'mediawiki.String' ).codePointLength;
+	function visibleCodePointLimit( textInputWidget ) {
+		const limit = +textInputWidget.$input.attr( 'maxlength' );
+		const codePointLength = require( 'mediawiki.String' ).codePointLength;
 
 		function updateCount() {
-			var value = textInputWidget.getValue(),
-				remaining;
-			remaining = limit - codePointLength( value );
-			if ( remaining > 99 ) {
-				remaining = '';
-			} else {
-				remaining = mw.language.convertNumber( remaining );
-			}
-			textInputWidget.setLabel( remaining );
+			const value = textInputWidget.getValue();
+			const remaining = limit - codePointLength( value );
+			const label = remaining > 99 ? '' : mw.language.convertNumber( remaining );
+			textInputWidget.setLabel( label );
 		}
 		textInputWidget.on( 'change', updateCount );
 		// Initialise value
 		updateCount();
-	};
+	}
 
-	$( function () {
-		var $checkboxes = $( '#powersearch input[id^="mw-search-ns"]' );
-
+	$( () => {
 		$( '.ext-replacetext-invert' ).on( 'click', invertSelections );
 
 		// Attach handler for check all/none buttons
-		$( '#mw-search-toggleall' ).on( 'click', function () {
+		const $checkboxes = $( '#powersearch input[id^="mw-search-ns"]' );
+		$( '#mw-search-toggleall' ).on( 'click', () => {
 			$checkboxes.prop( 'checked', true );
 		} );
-		$( '#mw-search-togglenone' ).on( 'click', function () {
+		$( '#mw-search-togglenone' ).on( 'click', () => {
 			$checkboxes.prop( 'checked', false );
 		} );
 
-		var $wpSummary = $( '#wpSummary' );
+		const $wpSummary = $( '#wpSummary' );
 		if ( $wpSummary.length ) {
 			// Show a byte-counter to users with how many bytes are left for their edit summary.
 			visibleCodePointLimit( OO.ui.infuse( $wpSummary ) );
